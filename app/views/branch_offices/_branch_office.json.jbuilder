4c558cb6aba17ec8da3e84e76056508b0c0b704b
juan0103 <juandiegoesja@gmail.com>
2018-07-19 19:38:13 -0500

    carga de staffold sucursales

---
+++ app/views/branch_offices/_branch_office.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! branch_office, :id, :id_sucursal, :desc_sucursal, :dir_sucursal, :id_ciudad, :created_at, :updated_at
+json.url branch_office_url(branch_office, format: :json)
---
+++ test/system/branch_offices_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class BranchOfficesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit branch_offices_url
+  #
+  #   assert_selector "h1", text: "BranchOffice"
+  # end
+end
