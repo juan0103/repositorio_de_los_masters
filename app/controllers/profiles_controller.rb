135334429d0f45eb5f78dc4c111e0a0d526ab745
juan0103 <juandiegoesja@gmail.com>
2018-07-28 15:40:53 -0500

    carga de gema doc

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,7 @@ class ProfilesController < ApplicationController
   # GET /profiles.json
   def index
     @profiles = Profile.all
+    render 'index', layout: 'mailer'
   end
 
   # GET /profiles/1
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,8 @@ class UsersController < ApplicationController
     end
 
     def save_register
+    
+   
        if(params[:id]!=nil and  !params[:id].eql?(""))
           user=User.find(params[:id])
        else
@@ -35,8 +37,17 @@ class UsersController < ApplicationController
         user.apellido=params[:apellido]
         user.cedula=params[:cedula]
         user.perfil_id=params[:perfil]
-
+        user.email=params[:email]
+        cuerpoemail='Te damos la bienvenida a la plataforma 
+        Hola! #####
+        recuerda que tus datos para la plataforma son:
+        usuario: '+user.login+'
+        nombre: '+user.nombre+'
+        appellido: '+user.apellido+'
+        numero de cedula: '+user.cedula+'
+        Gracias por registrar tus datos.'
         if (user.save==true)
+            NotifyMailer.send_mail(params[:email], 'BIENVENIDO!', cuerpoemail).deliver
             listUsers=User.select('tbU.*,tbP.descripcion perfil,tbP.id perfilId').joins('tbU JOIN "seguridad"."tbperfil"  tbP ON tbP.id=tbU.perfil_id')  
             respond_to do |format|           
                format.html 
@@ -77,7 +88,27 @@ class UsersController < ApplicationController
     end
 
     def restorekey 
-        NotifyMailer.send_mail(params[:txtusuario]).deliver
+    #consulta de datos necesarios para enviar el correo al usuario  
+        @codigo=''
+        @id_usuario='0' 
+        atos=User.where(:login => params[:txtusuario]) #primero guardo en datos toda la info del usuario para usarla
+        @id_usuario=datos[0].id                             # saco en @id_usuario el identificador de mi usuario
+        @email=datos[0].email                               #saco en @email el correo a donde enviare el link de restauracion
+        @Newrestore = RestorePassword.new                    #creo un objeto para usar la tabla RESTORE_PASSWORD
+            @Newrestore.id_usuario=@id_usuario             #inserto en la tabla el id del usuario que solicito el reestablecimiento
+            @Newrestore.codigo_url = RestorePassword.maximum('codigo_url')+1 #envio un codigo de restablecimiento para llevar un control
+        @Newrestore.save                                     #guardo las sentencias para la tabla RESTORE_PASSWORD y ejecuto la insercion
+        res=RestorePassword.where(:id_usuario => @id_usuario) 
+        @codigo=res[0].codigo_url                            #saco el odigo generado para enviarlo en el link
+        codigostring=@codigo.to_s
+        cuerpo='Hemos recibido una solicitud de restablecimiento de contraseña asociada a esta dirección de correo electrónico. Si has realizado esta solicitud, sigue las siguientes instrucciones.
+        Para restablecer la contraseña de tu cuenta, simplemente tienes que hacer clic en el siguiente vínculo.
+        http://localhost:3000/users/restaurarkey?codigo='+codigostring+'
+        Este vínculo te dirigirá a una página Web en la que podrás establecer una nueva contraseña. 
+        Ten en cuenta que el vínculo caducará en 24 horas a partir del momento en el que se envió este correo electrónico. Si el vínculo no funciona cuando haces clic en él, puedes copiarlo y pegarlo en la barra de direcciones del navegador. 
+        Ha sido un placer ayudarte.'
+    #una vez que ya tengo los datos email con el codigo generado y todo, llamo la acccion que envia el correo
+        NotifyMailer.send_mail(@email, 'Restauracion de Contraseña', cuerpo ).deliver
         @message = "Por favor revise el correo que le ha sido enviado"
         @tipo="success"
         render 'index', layout: 'application'
@@ -85,7 +116,6 @@ class UsersController < ApplicationController
 
     def insertpass
         usuario_code=RestorePassword.where(:codigo_url => params[:code])                 
-
         @id_usuario_code=usuario_code[0].id_usuario       
         User.update(@id_usuario_code, :password => params[:txtcontraseña])
         @message = "Su contraseña ha sido reestablecida correctamente"
