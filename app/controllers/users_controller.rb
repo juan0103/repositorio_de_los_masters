6642f092f75f3160e428b31f3f370ea6011605c9
juan0103 <juandiegoesja@gmail.com>
2018-08-20 11:03:13 -0500

    confirmadoo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -100,7 +100,7 @@ class UsersController < ApplicationController
     #consulta de datos necesarios para enviar el correo al usuario  
         @codigo=''
         @id_usuario='0' 
-        atos=User.where(:login => params[:txtusuario]) #primero guardo en datos toda la info del usuario para usarla
+        datos=User.where(:login => params[:txtusuario]) #primero guardo en datos toda la info del usuario para usarla
         @id_usuario=datos[0].id                             # saco en @id_usuario el identificador de mi usuario
         @email=datos[0].email                               #saco en @email el correo a donde enviare el link de restauracion
         @Newrestore = RestorePassword.new                    #creo un objeto para usar la tabla RESTORE_PASSWORD
@@ -110,13 +110,8 @@ class UsersController < ApplicationController
         res=RestorePassword.where(:id_usuario => @id_usuario) 
         @codigo=res[0].codigo_url                            #saco el odigo generado para enviarlo en el link
         codigostring=@codigo.to_s
-        cuerpo='Hemos recibido una solicitud de restablecimiento de contraseña asociada a esta dirección de correo electrónico. Si has realizado esta solicitud, sigue las siguientes instrucciones.
-        Para restablecer la contraseña de tu cuenta, simplemente tienes que hacer clic en el siguiente vínculo.
-        http://localhost:3000/users/restaurarkey?codigo='+codigostring+'
-        Este vínculo te dirigirá a una página Web en la que podrás establecer una nueva contraseña. 
-        Ten en cuenta que el vínculo caducará en 24 horas a partir del momento en el que se envió este correo electrónico. Si el vínculo no funciona cuando haces clic en él, puedes copiarlo y pegarlo en la barra de direcciones del navegador. 
-        Ha sido un placer ayudarte.'
-    #una vez que ya tengo los datos email con el codigo generado y todo, llamo la acccion que envia el correo
+        cuerpo="http://localhost:3000/users/restaurarkey?codigo=#{codigostring}"
+        #una vez que ya tengo los datos email con el codigo generado y todo, llamo la acccion que envia el correo
         NotifyMailer.send_mail(@email, 'Restauracion de Contraseña', cuerpo ).deliver
         @message = "Por favor revise el correo que le ha sido enviado"
         @tipo="success"
